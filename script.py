3ff49b16245d819d58097ac47b37a0f2c2c1c28b
Henriquesoares192 <Henriquebriet0@gmail.com>
2025-09-18 18:35:16 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,17 @@
-import serial
 import boto3
-import time
 
-# Configuração da porta serial do ATtiny85
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+# Usando credenciais diretamente (não recomendado para produção)
+ec2 = boto3.client(
+    'ec2',
+    aws_access_key_id='SUA_ACCESS_KEY',
+    aws_secret_access_key='SUA_SECRET_KEY',
+    region_name='us-east-1'
+)
 
-# Configuração AWS
-ec2 = boto3.client('ec2', region_name='us-east-1')
-
-while True:
-    if ser.in_waiting:
-        line = ser.readline().decode().strip()
-        if line == "OPEN_KALI_CLOUD":
-            print("Comando recebido! Abrindo Kali Linux...")
-            
-            # Criar instância EC2 (exemplo genérico)
-            response = ec2.run_instances(
-                ImageId='ami-0xxxxxxxxxxxxxx', # Kali Linux AMI
-                InstanceType='t2.micro',
-                MinCount=1,
-                MaxCount=1,
-                KeyName='sua-chave-aws',
-                SecurityGroupIds=['sg-xxxxxxxx'],
-                SubnetId='subnet-xxxxxxxx'
-            )
-            instance_id = response['Instances'][0]['InstanceId']
-            print(f"Instância criada: {instance_id}")
+response = ec2.run_instances(
+    ImageId='ami-0xxxxxxxxxxxxxx',  # Kali Linux AMI
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1
+)
+print("Instância criada:", response['Instances'][0]['InstanceId'])
